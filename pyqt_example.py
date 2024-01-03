cdb136cdc8bec1f4cb62bbbdd4976a7c50c0d4db
cmolarti <cmolarti@gmail.com>
2024-01-02 23:53:36 -0500

    created the age calculator that contains, labels, lineedit, push button, qapplication, qwidget, self.setWindowTitle(), gridlayout, connect push button to a function

--- pyqt_example.py
+++ pyqt_example.py
@@ -1,25 +1,46 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QGridLayout, QLineEdit
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, \
+QGridLayout, QLineEdit, QPushButton
 import sys
+from datetime import datetime
 
 
 class AgeCalc(QWidget):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle('Age Calculator')
         grid = QGridLayout()
 
+        # Create Widgets
         name_lbl = QLabel('Name')
-        name_txtbox = QLineEdit()
+        self.name_txtbox = QLineEdit()
 
         birthdate_lbl = QLabel('Birthday (MM/DD/YYYY)')
-        birthdate_txtbox = QLineEdit()
+        self.birthdate_txtbox = QLineEdit()
 
+        calc_btn = QPushButton('Calculate Age')
+        calc_btn.clicked.connect(self.calculate_age)
+        self.output_lbl = QLabel('')
+
+        # Attach Widgets to Grid
         grid.addWidget(name_lbl, 0, 0)
-        grid.addWidget(name_txtbox, 0, 1)
+        grid.addWidget(self.name_txtbox, 0, 1)
         grid.addWidget(birthdate_lbl, 1, 0)
-        grid.addWidget(birthdate_txtbox, 1, 1)
+        grid.addWidget(self.birthdate_txtbox, 1, 1)
+        grid.addWidget(calc_btn, 2, 0, 1, 2)   #row, col, span of rows, span of columns
+        grid.addWidget(self.output_lbl, 3, 0, 1, 2)
 
         self.setLayout(grid)
 
+    def calculate_age(self):
+        today = datetime.today().year
+        print(f'today: {today}')
+        
+        birthdate = datetime.strptime(self.birthdate_txtbox.text() , '%m/%d/%Y').date().year
+        print(f'birthdate: {birthdate}')
+
+        age = today - birthdate
+        print(f'age: {age}')
+        self.output_lbl.setText(f'{self.name_txtbox.text()} is {age} years old')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
